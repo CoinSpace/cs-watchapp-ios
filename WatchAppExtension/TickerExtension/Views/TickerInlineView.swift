36f3b369a7b6f5ca82f6b99886a13f94840318ab
nikashitsa <vernik91@gmail.com>
2025-09-01 15:25:25 +0200

    wip

--- WatchAppExtension/TickerExtension/Views/TickerInlineView.swift
+++ WatchAppExtension/TickerExtension/Views/TickerInlineView.swift
@@ -5,7 +5,7 @@ struct TickerInlineView: View {
     
     var body: some View {
         if let price = entry.cryptoItem.ticker?.price {
-            Text("\(entry.cryptoItem.crypto.symbol) \(AppService.shared.formatFiat(price, entry.cryptoItem.currency.rawValue, true))")
+            Text(verbatim: "\(entry.cryptoItem.crypto.symbol) \(AppService.shared.formatFiat(price, entry.cryptoItem.currency.rawValue, true))")
         } else {
             Text(verbatim: "...")
         }
